81ef57ae98e842af6efbb816be03de1bf4a28f66
Aleksandra Zelenkova <sashazelenkova39@gmail.com>
2024-03-27 02:15:57 +0300

    статьи для фильмов

--- src/javascript/cards.js
+++ src/javascript/cards.js
@@ -1,6 +1,73 @@
-$(document).ready(function () {
-    $('.A_turnover_button').click(function () {
-        $('.O_FlippedCard').removeClass('none');
-        $('.O_card_turn').addClass('none');
+document.ready(function() {
+
+// Получаем все чекбоксы с классом 'btn'
+let checkboxes = document.querySelectorAll('#S_ShortCardBlock .btn');
+
+// Добавляем обработчик события клика для каждого чекбокса
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', function() {
+        let filter = this.parentElement.querySelector('p').textContent.toLowerCase(); // Получаем текст из тега <p> рядом с чекбоксом
+        let items = document.querySelectorAll('#S_ShortCardBlock > .W_shorts_cards > .O_CardForLearningWithHastag:not(.M_BlockSeparator)'); // Выбираем все элементы с классом 'O_CardForLearningWithHastag', кроме блоков с классом 'M_BlockSeparator'
+        
+        items.forEach(item => {
+            if (item.querySelector('.A_section_card_title p').textContent.toLowerCase().includes(filter)) {
+                if (item.style.display === 'none') {
+                    item.style.display = 'block'; // Показываем элемент, если он скрыт
+                } else {
+                    item.style.display = 'none'; // Скрываем элемент, если он видим
+                }
+            }
+        });
     });
+});
+
+
+
+  //   filterSelection("all")
+  // function filterSelection(c) {
+  //   var x, i;
+  //   x = document.getElementsByClassName("O_CardForLearningWithHastag");
+  //   if (c == "all") c = "";
+  //   // Добавить класс "show" (display:block) к отфильтрованным элементам и удалите класс "show" из элементов, которые не выбраны
+  //   for (i = 0; i < x.length; i++) {
+  //     w3RemoveClass(x[i], "show");
+  //     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  //   }
+  // }
+
+  // // Показать отфильтрованные элементы
+  // function w3AddClass(element, name) {
+  //   var i, arr1, arr2;
+  //   arr1 = element.className.split(" ");
+  //   arr2 = name.split(" ");
+  //   for (i = 0; i < arr2.length; i++) {
+  //     if (arr1.indexOf(arr2[i]) == -1) {
+  //       element.className += " " + arr2[i];
+  //     }
+  //   }
+  // }
+
+  // // Скрыть элементы, которые не выбраны
+  // function w3RemoveClass(element, name) {
+  //   var i, arr1, arr2;
+  //   arr1 = element.className.split(" ");
+  //   arr2 = name.split(" ");
+  //   for (i = 0; i < arr2.length; i++) {
+  //     while (arr1.indexOf(arr2[i]) > -1) {
+  //       arr1.splice(arr1.indexOf(arr2[i]), 1);
+  //     }
+  //   }
+  //   element.className = arr1.join(" ");
+  // }
+
+  // // Добавить активный класс к текущей кнопке управления (выделите ее)
+  // var btnContainer = document.getElementById("W_filter");
+  // var btns = btnContainer.getElementsByClassName("btn");
+  // for (var i = 0; i < btns.length; i++) {
+  //   btns[i].addEventListener("click", function() {
+  //     var current = document.getElementsByClassName("active");
+  //     current[0].className = current[0].className.replace(" active", "");
+  //     this.className += " active";
+  //   });
+  // }
 })
